1dc0ed9c7cf52dec1a0d95e336bb158a3e7bcb86
HarukaDev <harukakasugano31@gmail.com>
2025-11-26 18:29:34 -0500

    fix

--- app/Console/Commands/CursoCommand.php
+++ app/Console/Commands/CursoCommand.php
@@ -51,7 +51,9 @@ class CursoCommand extends Command
             Log::info('Fetching data from the database...');
             $campanas = DB::table($this->table_campana)
                 ->whereNull('Fe_Borrado')
-                ->where('Fe_Fin', '<=', date('Y-m-d'))
+                //where month of Fe_Fin is september or october or november
+                ->whereMonth('Fe_Fin', '=', 9)
+
                 ->get();
             //get pedidos id_campana in campanas send_constancia is PENDING and tipo_curso is 1 and Nu_Estado is 2 and join with entidad and get Nu_Celular_Entidad
             $pedidos = DB::table($this->table_pedido_curso)
